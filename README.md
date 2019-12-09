1321d8fe7f1a02cd9d9992b94e5829c0778d9098
Anthie123 <andrej.thiele@conciso.de>
2019-12-09 11:35:50 +0100

    initial version of xmas tree kata

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# coding-dojo
-This is the base project for the coding dojos. It can be used as blue print to 
-project branches.
+# Code Kata Xmas tree
+In this code kata we create an xmas tree as ascii art. An xmas tree with the branch size 5 should be drawn
+like this:
+
+```
+    X
+   XXX
+  XXXXX
+ XXXXXXX
+XXXXXXXXX
+    I
+```
+Additionally there could be a star on the top of the tree:
+```
+    *
+    X
+   XXX
+  XXXXX
+ XXXXXXX
+XXXXXXXXX
+    I
+```
+
+The exercise is to create a tree with variable size of branches and with or without star at the top.
---
+++ src/main/java/de/conciso/codingdojo/XmasTreeDrawer.java
@@ -0,0 +1,21 @@
+package de.conciso.codingdojo;
+
+public interface XmasTreeDrawer {
+    /**
+     * The function creates an ascii art xmas tree as String. The result can be used to draw a tree on the screen.
+     *
+     * Example:
+     * A tree with 3 branches with star will be created like this:
+     *
+     * "  *\n
+     *    x\n
+     *   xxx\n
+     *  xxxxx\n
+     *   I"
+     *
+     * @param numberOfBranches number of branches of the Xmas tree
+     * @param withStar <code>true</code> if a star should be drawn at the top of the Xmas tree, <code>false</code>
+     *                 otherwise
+     */
+    public String drawXmasTree(int numberOfBranches, boolean withStar);
+}
