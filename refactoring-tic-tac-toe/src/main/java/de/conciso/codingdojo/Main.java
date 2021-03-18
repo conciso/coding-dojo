064e05cde26ef0a0c6c62b9871767a8db144f217
Andrej Thiele <andrej.thiele@conciso.de>
2021-03-18 17:50:26 +0100

    added gui if and main class

--- refactoring-tic-tac-toe/src/main/java/de/conciso/codingdojo/Main.java
+++ refactoring-tic-tac-toe/src/main/java/de/conciso/codingdojo/Main.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
   public void run() throws IOException {
+
   }
 
   public static void main(String[] args) throws IOException {
---
+++ refactoring-tic-tac-toe/src/main/java/de/conciso/codingdojo/TicTacToeGui.java
@@ -0,0 +1,16 @@
+package de.conciso.codingdojo;
+
+/**
+ * The game should work with a simple GUI interface. This should have following behaviour:
+ *
+ * 1. Ask for the names of the players
+ * 2. Ask for the size of the game grid
+ * 3. Show the grid and the players with their game tile and their number of moves.
+ * 4. Ask in changing order for the game move of the actual player and then repeat with step 3.
+ * 5. If a player has won the game show the name of the winner together with the needed moves.
+ * 6. Ask if the game should be played again and restart at step 1 if needed.
+ */
+public interface TicTacToeGui {
+
+  // TODO
+}
