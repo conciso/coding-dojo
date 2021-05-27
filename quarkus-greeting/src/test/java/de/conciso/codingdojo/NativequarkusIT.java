e8ffed9fc24f5900e63bad0b958b4d363a4f54bd
Anthie123 <andrej.thiele@conciso.de>
2021-05-27 17:27:45 +0200

    Added quarkus-greeting kata

---
+++ quarkus-greeting/src/test/java/de/conciso/codingdojo/NativequarkusIT.java
@@ -0,0 +1,9 @@
+package de.conciso.codingdojo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativequarkusIT extends WelcomeControllerTest {
+
+    // Execute the same tests but in native mode.
+}
