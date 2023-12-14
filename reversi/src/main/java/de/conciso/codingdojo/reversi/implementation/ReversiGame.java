a6bff667e9fe22caafac1100c5786538134fe11b
Andrej <andrej.thiele@conciso.de>
2023-12-14 21:19:15 +0100

    Reversi Kata vom 14.12.2023

--- reversi/src/main/java/de/conciso/codingdojo/reversi/implementation/ReversiGame.java
+++ reversi/src/main/java/de/conciso/codingdojo/reversi/implementation/ReversiGame.java
@@ -1,12 +1,16 @@
 package de.conciso.codingdojo.reversi.implementation;
 
+import de.conciso.codingdojo.reversi.AlreadyOccupiedException;
 import de.conciso.codingdojo.reversi.Board;
+import de.conciso.codingdojo.reversi.Disc;
 import de.conciso.codingdojo.reversi.Player;
 import de.conciso.codingdojo.reversi.Position;
 import de.conciso.codingdojo.reversi.Reversi;
 import de.conciso.codingdojo.reversi.implementation.CharacterDisc;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class ReversiGame implements Reversi<Character, CharacterDisc> {
 
@@ -42,11 +46,40 @@ public class ReversiGame implements Reversi<Character, CharacterDisc> {
 
   @Override
   public List<Position> calculateMoves(Player<?> player) {
+    Disc<Character> disk = player.disc();
+    Set<Position> result = new
+    for (int i = 0; i < board.getMaximumPosition().x(); i++) {
+      for (int j = 0; j < board.getMaximumPosition().y(); j++) {
+        Position position = new Position(i, j);
+        if (board.getPosition(position).isPresent() && board.getPosition(position).get().equals(disk)) {
+          for (int n = 0; n < 9; n++) {
+            var x = n % 3 - 1;
+            var y = n / 3 - 1;
+            var pos = new Position(x + i, y + j);
+            if (board.getPosition(pos).isPresent() && !board.getPosition(pos).get().equals(disk)) {
+              var step = 1;
+              while (board.getPosition(new Position(x * step + i, y * step + j)).isPresent() && !board.getPosition(
+                  new Position(x * step + i, y * step + j)).get().equals(disk)){
+                step++;
+
+              }
+              if (board.getPosition(new Position(x * step + i, y * step + j)).isEmpty() ){
+
+              }
+            }
+          }
+          if (board.getPosition())
+        }
+      }
+    }
     return null;
   }
 
   @Override
   public void doMove(Position position) {
+    if (board.getPosition(position).isPresent()) {
+      throw new AlreadyOccupiedException();
+    }
     this.board.setPosition(position, currentPlayer.disc());
     switchPlayer();
   }
--- reversi/src/test/java/de/conciso/codingdojo/reversi/ReversiGameTest.java
+++ reversi/src/test/java/de/conciso/codingdojo/reversi/ReversiGameTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import de.conciso.codingdojo.reversi.implementation.CharacterDisc;
 import de.conciso.codingdojo.reversi.implementation.ReversiGame;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,10 +20,12 @@ class ReversiGameTest {
 
   public static final CharacterDisc CHARACTER_DISC_P1 = new CharacterDisc('W');
   public static final CharacterDisc CHARACTER_DISC_P2 = new CharacterDisc('B');
+  public static final Position FIRSTVALIDPOSITION = new Position(4, 2);
   Player player1 = new Player("Player1", CHARACTER_DISC_P1);
   Player player2 = new Player("Player2", CHARACTER_DISC_P2);
 
   ReversiGame cut;
+
   @Nested
   class UsingConstructor {
 
@@ -39,10 +43,10 @@ class ReversiGameTest {
   }
 
   @Nested
-  class UsingGetBoard{
+  class UsingGetBoard {
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
       cut = new ReversiGame(player1, player2);
     }
 
@@ -52,25 +56,25 @@ class ReversiGameTest {
     }
 
     @Test
-    void returnsBoardWithProperMaxBoardDimensions(){
-      assertThat(cut.getBoard().getMaximumPosition()).isEqualTo(new Position(7,7));
+    void returnsBoardWithProperMaxBoardDimensions() {
+      assertThat(cut.getBoard().getMaximumPosition()).isEqualTo(new Position(7, 7));
     }
 
     @Test
-    void returnsBoardWithProperMinBoardDimensions(){
-      assertThat(cut.getBoard().getMinimumPosition()).isEqualTo(new Position(0,0));
+    void returnsBoardWithProperMinBoardDimensions() {
+      assertThat(cut.getBoard().getMinimumPosition()).isEqualTo(new Position(0, 0));
     }
 
     @ParameterizedTest
     @MethodSource("de.conciso.codingdojo.reversi.ReversiGameTest#startingStateParams")
-    void returnBoardWithCorrectStartingState(int x, int y, Optional<CharacterDisc> result){
-      assertThat(cut.getBoard().getPosition(new Position(x,y))).isEqualTo(result);
+    void returnBoardWithCorrectStartingState(int x, int y, Optional<CharacterDisc> result) {
+      assertThat(cut.getBoard().getPosition(new Position(x, y))).isEqualTo(result);
     }
 
     @ParameterizedTest
     @MethodSource("de.conciso.codingdojo.reversi.ReversiGameTest#startingEmptyStateParams")
-    void returnBoardWithCorrectEmptyFields(int x, int y){
-      assertThat(cut.getBoard().getPosition(new Position(x,y))).isEmpty();
+    void returnBoardWithCorrectEmptyFields(int x, int y) {
+      assertThat(cut.getBoard().getPosition(new Position(x, y))).isEmpty();
     }
   }
 
@@ -82,7 +86,8 @@ class ReversiGameTest {
         Arguments.arguments(4, 4, Optional.of(CHARACTER_DISC_P1))
     );
   }
-  private static Stream<Arguments> startingEmptyStateParams(){
+
+  private static Stream<Arguments> startingEmptyStateParams() {
     return Stream.of(
         Arguments.arguments(2, 2),
         Arguments.arguments(2, 3),
@@ -114,42 +119,62 @@ class ReversiGameTest {
 
     @Test
     void checkIfPlayerSwitchAfterMove() {
-      Player<?> player1 = cut.getCurrentPlayer();
-      cut.doMove(null);
-      Player<?> player2 = cut.getCurrentPlayer();
-      assertThat(player1).isNotEqualTo(player2);
+      Player<?> currentPlayer = cut.getCurrentPlayer();
+      cut.doMove(FIRSTVALIDPOSITION);
+      Player<?> nextCurrentPlayer = cut.getCurrentPlayer();
+      assertThat(currentPlayer).isNotEqualTo(nextCurrentPlayer);
     }
 
     @Test
     void checkIfPlayerSwitchAfterTwoMoves() {
-      cut.doMove(null);
-      cut.doMove(null);
+      cut.doMove(FIRSTVALIDPOSITION);
+      cut.doMove(new Position(3, 2));
       Player<?> currentPlayer = cut.getCurrentPlayer();
       assertThat(currentPlayer).isEqualTo(player1);
     }
   }
 
   @Nested
-  class UsingDoMove{
+  class UsingDoMove {
+
     @BeforeEach
     void setUp() {
       cut = new ReversiGame(player1, player2);
     }
 
     @Test
-    void verifyThatTheBoardChanges(){
-      Position position = new Position(0,0);
+    void verifyThatTheBoardChanges() {
+      Position position = new Position(0, 0);
       cut.doMove(position);
       assertThat(cut.getBoard().getPosition(position)).isEqualTo(Optional.of(player1.disc()));
     }
 
     @Test
     void verifyThatPositionIsNotAlreadyOccupied() {
-      Position position = new Position(0, 0);
+      Position position = FIRSTVALIDPOSITION;
       cut.doMove(position);
-      assertThatExceptionOfType(AlreadyOccupiedException.class).isThrownBy(()->cut.doMove(position));
+      assertThatExceptionOfType(AlreadyOccupiedException.class).isThrownBy(() -> cut.doMove(position));
     }
 
+  }
+
+  @Nested
+  class UsingCalculateMoves {
+
+
+    @BeforeEach
+    void setUp() {
+      cut = new ReversiGame(player1, player2);
+    }
+
+    @Test
+    void verifyThatStartingMovesAreCorrect() {
+      List<Position> validList = List.of(new Position(4, 2), new Position(5, 3), new Position(2, 6),
+          new Position(3, 7));
+      List<Position> resultList = cut.calculateMoves(player1);
+      assertThat(resultList).containsAll(validList);
+
+    }
   }
 
 }
